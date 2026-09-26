05e11d942aab6cf9ae94ac72b212c7632a98725c
Balaji V <balajivenkatesh132005@gmail.com>
2026-09-26 09:35:24 +0000

    Update application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,1 @@
-package com.example;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from End-to-End CI/CD Pipeline!");
-    }
-}
+System.out.println("Hello from End-to-End CI/CD Pipeline!")
